223cb1d57bd4eef462edfe20de8c9cf5058ed3ba
Lukasz <leszman.lukasz@gmail.com>
2023-08-09 17:15:39 +0200

    i am Lucas

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+//    static char space = 32;
+//    static char hashtag = 35;
+    public static void main(String[] args) {
+        Variables variables = new Variables();
+
+        //i am trying to be best Lucas
+        System.out.println(variables.I + variables.A + variables.M + variables.T + variables.R + variables.Y +
+                           variables.I + variables.N + variables.G + variables.T + variables.O + variables.B +
+                           variables.E + variables.B + variables.E + variables.S + variables.T + variables.L +
+                           variables.U + variables.C + variables.A + variables.S);
+        //be the best Lucas i can be
+        System.out.println(variables.B + variables.E + variables.T + variables.H + variables.E + variables.B +
+                           variables.E + variables.S + variables.T + variables.L + variables.U + variables.C +
+                           variables.A + variables.S + variables.I + variables.C + variables.A + variables.N +
+                           variables.B + variables.E);
+        //i am still working on myself
+        System.out.println(variables.I + variables.A + variables.M + variables.S + variables.T + variables.I +
+                           variables.L + variables.L + variables.W + variables.O + variables.R + variables.K +
+                           variables.I + variables.N + variables.G + variables.O + variables.N + variables.M +
+                           variables.Y + variables.S + variables.E + variables.L + variables.F);
+    }
+}
